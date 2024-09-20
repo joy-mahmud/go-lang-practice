3f82435d0f63341f52d51b15ebbb978e88519ed0
joy-mahmud <joymahmudiucse@gmail.com>
2024-09-20 23:42:13 +0600

    multiple constants declaration

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+const (
+	A int = 1
+	B int = 2
+	c int = 3
+)
+
 func main() {
+	var num1 int32 = 120
+	var num2 int16 = 125
+	fmt.Println(num1, num2)
+	fmt.Println(A, B, c)
 	fmt.Println("hello world")
 }
